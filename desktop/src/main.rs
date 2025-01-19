1c97aed5eb585c9456d76091d37f10ed4bd93840
Marley Reeves <marley@marlstar.com>
2025-01-19 17:03:43 +1000

    load rom from file

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,6 +1,7 @@
 use chip8_core::Emu;
 use desktop::constants::*;
 use std::env;
+use std::io::Read;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -8,5 +9,14 @@ fn main() {
     if args.len() != 2 {
         println!("Usage: cargo run path/to/rom");
         return;
+    }
+
+    let mut chip8 = Emu::new();
+
+    { // Load the rom
+        let mut rom = std::fs::File::open(&args[1]).expect("Unable to open rom");
+        let mut buffer = Vec::new();
+        rom.read_to_end(&mut buffer).unwrap();
+        chip8.load_rom(&buffer);
     }
 }
